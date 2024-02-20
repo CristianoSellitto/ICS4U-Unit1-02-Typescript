f0284e9b89b552c17792fe6edb87c6d281b7daaa
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2024-02-20 09:49:17 -0500

    Added error check

--- index.ts
+++ index.ts
@@ -1,16 +1,27 @@
 /**
-* Finds how much logs a truck can carry if the max weight it can carry is 1100 kg and each log is 20 kg/m
+*
+* This program calculates the energy an object can release
 *
 * By:      Cristiano Sellitto
 * Version: 1.0
 * Since:   2024-02-15
-*/ 
+*/
 
 import { createPrompt } from 'bun-promptx'
 
-const mass = createPrompt('Enter mass (kg): ')
-const energyReleased = (parseFloat(mass.value) * ((2.998 * (10**8))**2)).toExponential()
+// Mass input
+const massString = createPrompt('Enter mass (kg): ')
 
-console.log(`A ${mass.value} kg object would produce ${energyReleased} J of energy.`)
+// Mass float and error check
+const mass = parseFloat(massString.value)
+if (isNaN(mass) == true || mass < 0) {
+  // Invalid input
+  console.log('Invalid input.')
+} else {
+  // Valid input
+  const energyReleased = (mass * ((2.998 * (10**8))**2)).toExponential()
+  console.log(`A ${mass} kg object would produce ${energyReleased} J of energy.`)
+}
 
+// Show the program as done
 console.log('\nDone.')
